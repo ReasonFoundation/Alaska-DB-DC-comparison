4d727fcbc04caafb0a9966d4bdfb11e7666ab385
sbhaga5 <67068151+sbhaga5@users.noreply.github.com>
2023-03-07 16:36:53 -0500

    Update Alaska.R

--- Alaska.R
+++ Alaska.R
@@ -2,24 +2,31 @@ rm(list = ls())
 library("readxl")
 library(tidyverse)
 library(dplyr)
+library(zoo)
 source('Functions.R')
 FileName <- 'Alaska Inputs.xlsx'
 model_inputs <- read_excel(FileName)
 
-GetComparisonData <- function(Age = 31:60,
+GetComparisonData <- function(EntryAge = 31,
+                              YOSRange = 30,
                               DCReturn = 0.07,
                               PayoutRate = 0.0589,
                               PayrollGrowth = 0.0275,
-                              RetAge = 60,
-                              LifeExpectancy = 85){
+                              LifeExpectancy = 85,
+                              EmpType = 'Teachers'){
   
+  Age <- EntryAge:(EntryAge + YOSRange - 1)
   Data <- expand_grid(Age) %>%
-    mutate(YOS = 1:30) %>%
+    mutate(YOS = 1:YOSRange) %>%
     left_join(model_inputs, by = "YOS") %>%
     group_by(EmployeeType) %>%
-    mutate(CumMult = (1+PayrollGrowth)^(YOS-1),
+    mutate(RetAgeOther = ifelse(YOS >= 30, Age, 60),
+           RetAgeTeacherPolice = ifelse(YOS >= 20, Age, 60),
+           RetAge = ifelse(EmployeeType == 'All Others', RetAgeOther, RetAgeTeacherPolice),
+           
+           CumMult = (1+PayrollGrowth)^(YOS-1),
            Salary = StartingSalary*CumMult,
-           FinalAvgSalary = rollmean(Salary, k = 5, fill = NA, align = "right"),
+           FinalAvgSalary = rollmean(Salary, k = SalPeriod, fill = NA, align = "right"),
            DBAnnuity = ifelse(YOS >= Vesting, Multiplier*YOS*FinalAvgSalary, 0),
            DBIncReplace = DBAnnuity/Salary,
            
@@ -33,7 +40,8 @@ GetComparisonData <- function(Age = 31:60,
            DCAnnuityRev = ifelse(DBAnnuity > 0, 
                                  PMT(r = PayoutRate, nper = LifeExpectancy - RetAge, pv = DCBalanceRev, t = 1) / 1.016725, 0)) %>%
     arrange(EmployeeType) %>%
-    ungroup()
+    ungroup() %>%
+    filter(EmployeeType == EmpType)
   
   return(Data)
 }
